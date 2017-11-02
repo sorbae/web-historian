06a78d7e1d7aa0effd02977444553bd837a0cbeb
Hack Reactor Student <student@hackreactor.com>
2017-11-01 21:44:57 -0700

    Create worker to download sites

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,16 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+var archive = require('../helpers/archive-helpers.js');
+
+archive.readListOfUrls(data => {
+  data.slice(0, -1).forEach(url => {
+    archive.isUrlArchived(url, urlIsInArchive => {
+      if (!urlIsInArchive) {
+        archive.downloadUrls([url]);
+      }
+    });
+  });
+});
+
+// */1 * * * * /Users/student/.nvm/versions/node/v6.11.3/bin/node /Users/student/code/hrsf85-web-historian/workers/htmlfetcher.js
